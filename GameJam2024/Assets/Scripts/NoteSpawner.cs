5f1ebca2e674901784148ee94caa08ab86754101
Yulnah <lea.pham31@gmail.com>
2024-01-23 14:51:26 +0100

    QTE Spawner OK

--- GameJam2024/Assets/Scripts/NoteSpawner.cs
+++ GameJam2024/Assets/Scripts/NoteSpawner.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class NoteSpawner : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject comboOne;
+
+    [SerializeField]
+    private float spawnInterval = 3.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnNoteRoutine(spawnInterval, comboOne));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Your update logic, if any
+    }
+
+    private IEnumerator SpawnNoteRoutine(float spawnInterval, GameObject notePrefab)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnInterval);
+
+            // Get the gameObject and create it at the position of this spawner
+            GameObject newNote = Instantiate(notePrefab, transform.position, Quaternion.identity);
+
+            // Set the new note as a child of the spawner's transform
+            newNote.transform.parent = transform;
+        }
     }
 }
