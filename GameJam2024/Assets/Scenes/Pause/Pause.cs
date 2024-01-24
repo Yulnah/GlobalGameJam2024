0c77be44646b058bc73b14e61333d82f3d1c7ac8
Yulnah <lea.pham31@gmail.com>
2024-01-24 22:20:58 +0100

    LD fix

--- GameJam2024/Assets/Scenes/Pause/Pause.cs
+++ GameJam2024/Assets/Scenes/Pause/Pause.cs
@@ -15,7 +15,7 @@ public class Pause : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if(Input.GetButtonDown("Pause"))
         {
             if(isPaused)
             {
